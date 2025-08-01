4b11cd3b7401b05c4c8933f47ae2f2cf2e08f544
Mary McCawley <mary@digitaldreamskc.com>
2025-07-31 22:25:32 -0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,25 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    eslint: {
-        ignoreDuringBuilds: true,
-    },
-    images: { unoptimized: true },
-    // Completely disable SWC to fix memory access error
-    swcMinify: false,
-    experimental: {
-        forceSwcTransforms: false,
-        esmExternals: false,
-    },
-    // Force use of Babel instead of SWC
-    webpack: (config, { dev, isServer }) => {
-        // Disable SWC loader
-        config.module.rules.forEach((rule) => {
-            if (rule.use && rule.use.loader === 'next-swc-loader') {
-                rule.use.loader = 'babel-loader';
-            }
-        });
-        return config;
-    },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    unoptimized: true,
+  },
+  swcMinify: false,
+  experimental: {
+    forceSwcTransforms: false,
+    esmExternals: false,
+  },
 };
 
 module.exports = nextConfig;
