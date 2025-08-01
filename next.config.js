911230b809750dfc992992f48afa27aa7fc8ce75
Mary McCawley <mary@digitaldreamskc.com>
2025-07-31 22:36:26 -0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,14 +3,14 @@ const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
-  images: {
-    unoptimized: true,
-  },
-  swcMinify: false,
+  images: { unoptimized: true },
+  swcMinify: false, // Still okay to keep this off
   experimental: {
     forceSwcTransforms: false,
     esmExternals: false,
   },
+  // ✅ Remove the webpack override block
 };
 
 module.exports = nextConfig;
+
